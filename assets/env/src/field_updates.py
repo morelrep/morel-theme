ad2c31355fab92c77a45afeb27ccd3d95e2865c2
febr3s <eduardofebresm@gmail.com>
2023-07-12 17:02:15 -0500

    fixed some minor issues

--- assets/env/src/field_updates.py
+++ assets/env/src/field_updates.py
@@ -1,5 +1,6 @@
 with open('../../../assets/data/books_zotero.csv','r') as file:
     filedata = file.read()
+    filedata = filedata.replace('Key','Id')
     filedata = filedata.replace('File Attachments','Cover')
     filedata = filedata.replace('Url','Download')
     filedata = filedata.replace('Library Catalog','Library')
